297a856bfc4446a7056425f85c6383ef7aa5437a
Ailson <backofficeailson-web@users.noreply.github.com>
2026-05-05 18:38:47 -0300

    conecta com backend online

--- server.js
+++ server.js
@@ -83,5 +83,5 @@ app.get("/", (req, res) => {
   res.send("FitControl Backend online");
 });
 app.listen(3000, () => {
-  console.log('Servidor rodando em http://https://fitcontrol-backend-vo77.onrender.com');
+  console.log('Servidor rodando em "https://fitcontrol-backend-wo77.onrender.com");
 });
